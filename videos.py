69f12a83ef0616566b8bd4c681b9851be585cc7b
Ceren <ceren.celik@metu.edu.tr>
2020-10-30 18:24:11 +0300

    Update videos.py

--- videos.py
+++ videos.py
@@ -3,16 +3,30 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
-while(True):
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+save = cv2.VideoWriter('save.avi', fourcc, 20.0, (640,480))
+
+while(cap.isOpened()):
     ret, frame = cap.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if ret==True:
+
+        print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        
+        save.write(frame)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    cv2.imshow('frame', frame)
-    cv2.imshow('gray', gray)
+        cv2.imshow('frame', frame)
+        cv2.imshow('gray', gray)
     
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
         break
 
 cap.release()
+out.release()
 cv2.destroyAllWindows()

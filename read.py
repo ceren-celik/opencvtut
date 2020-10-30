3a0c9832eace89dd2fbd7943eab4b0d940ae7a23
Ceren <ceren.celik@metu.edu.tr>
2020-10-30 16:56:22 +0300

    Update read.py

--- read.py
+++ read.py
@@ -1,8 +1,13 @@
 import cv2 
 
-img = cv2.imread('lena.jpg',0)
+img = cv2.imread('lena.jpg',1)
 print(img)
 
 cv2.imshow('image',img)
-cv2.waitKey(5000)
+k = cv2.waitKey(0) & 0xFF
 
+if k==27:
+    cv2.destroyAllWindows()
+elif k== ord('s'):
+    cv2.imwrite('lena_copy.png', img)
+    cv2.destroyAllWindows()

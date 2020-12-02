823d64e4c48f06ef7072f9d0c194ecf6e4e9e39e
Ceren <ceren.celik@metu.edu.tr>
2020-12-03 01:57:17 +0300

    smooth

--- smooth.py
+++ smooth.py
@@ -1,7 +1,23 @@
 import numpy as np
 import cv2
-from matplotlib import pyplot as pyplot
+from matplotlib import pyplot as plt
 
-img = cv2.imread('opencv-logo.png')
+img = cv2.imread('lena.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
+kernel = np.ones((5,5), np.float32)/25
+dst = cv2.filter2D(img, -1, kernel)
+blur = cv2.blur(img, (5,5))
+gblur = cv2.GaussianBlur(img, (5,5), 0)
+median = cv2.medianBlur(img, 5)
+bilateral = cv2.bilateralFilter(img, 9, 75, 75)
+
+titles = ['image', '2D Convolution', 'Blur', 'GBlur', 'Median', 'Bilateral']
+images = [img, dst, blur, gblur, median, bilateral]
+
+for i in range(6):
+    plt.subplot(2, 3, i+1), plt.imshow(images[i], 'gray')
+    plt.title(titles[i])
+    plt.xticks([]), plt.yticks([])
+
+plt.show()
